8560ff0a293c2bd325eb06bd531298e63840d75f
vassilpetkov <vassilpetkovv@abv.bg>
2015-01-07 13:16:16 +0200

    Fixed login and register

--- scripts/services/data/userData.js
+++ scripts/services/data/userData.js
@@ -2,26 +2,27 @@ app.factory('userData', ['$resource', 'baseServiceUrl', 'authentication',
     function($resource, baseServiceUrl, authentication){
 
     function registerUser(user){
-        return $resource(baseServiceUrl + 'user/register').save(user);
-        $resource.$promise.then(function (data) {
+        var resource = $resource(baseServiceUrl + 'user/register').save(user);
+        resource.$promise.then(function (data) {
             authentication.saveUser(data);
         });
+        return resource;
     }
 
     function loginUser(user){
-        return $resource(baseServiceUrl + 'user/login').save(user);
-
-        $resource.$promise.then(function (data) {
+        var resource = $resource(baseServiceUrl + 'user/login').save(user);
+        resource.$promise.then(function (data) {
             authentication.saveUser(data);
         });
+        return resource;
     }
 
-    function logoutUser(){
-        return $resource(baseServiceUrl + 'user/logout').save(user);
-        $resource.$promise.then(function () {
-            authentication.removeUser();
-        });
-    }
+    //function logoutUser(){
+    //    return $resource(baseServiceUrl + 'user/logout').save(user);
+    //    $resource.$promise.then(function () {
+    //        authentication.removeUser();
+    //    });
+    //}
 
     return {
         register: registerUser,
